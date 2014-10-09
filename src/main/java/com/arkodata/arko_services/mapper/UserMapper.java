92049bf6347eea14995a429acc909f7173b02b61
wbear <946010351@qq.com>
2014-10-09 11:11:43 +0800

    use mybatis annotations

--- src/main/java/com/arkodata/arko_services/mapper/UserMapper.java
+++ src/main/java/com/arkodata/arko_services/mapper/UserMapper.java
@@ -1,7 +1,10 @@
 package com.arkodata.arko_services.mapper;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.stereotype.Component;
 
 import com.arkodata.arko_services.domain.User;
 
